3cdf78164daccda7232c8b096282f137e36116f2
Fernando Poblete Arrau <fernandopobletearrau@gmail.com>
2024-01-25 23:20:47 -0300

    Test controller.

--- lib/src/test/java/logbook/controller/NoteControllerTest.java
+++ lib/src/test/java/logbook/controller/NoteControllerTest.java
@@ -1,6 +1,25 @@
 package logbook.controller;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import logbook.domain.Note;
+import logbook.service.NoteService;
+
 public class NoteControllerTest {
 
+    private NoteController noteController;
+	
+    private NoteService noteService;
+  
+    @Test
+    public void testFindAll() {
+        noteService = mock(NoteService.class);
+        noteController = new NoteController(noteService);
+		List<Note> result = noteController.allNotes();
+		verify(noteService).findAll();
 
+    }
 }
